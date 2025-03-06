fc22fa49f54b388a38e5e8740678410a2b02a530
Divine Mbamara <divinembamara@gmail.com>
2025-03-06 02:29:22 -0500

    Delete SecurePasswordApp directory

--- SecurePasswordApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SecurePasswordApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
