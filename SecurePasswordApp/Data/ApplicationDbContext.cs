8fb5ebff78396aa915b2354e5e3ac778b8db6e2e
Divine Mbamara <divinembamara@gmail.com>
2025-03-06 03:20:02 -0500

    Parent-child model & database migration

---
+++ SecurePasswordApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecurePasswordApp.Models;
+
+namespace SecurePasswordApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    //Users table in the database
+    public DbSet<User> Users { get; set; }
+
+    //PasswordCredentials table in the database
+    public DbSet<PasswordCredential> PasswordCredentials { get; set; }
+}
---
+++ SecurePasswordApp/Models/PasswordCredential.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace SecurePasswordApp.Models
+{
+    public class PasswordCredential
+    {
+        //PasswordCredentialId - primary key for Password Credential class
+        public int PasswordCredentialId { get; set; }
+
+
+        //WebsiteName - Field for users to enter the name of the website
+        [Required(ErrorMessage = "Please enter the Website name")]
+        [Display(Name = "Website Name")]
+        public string WebsiteName { get; set; }
+
+
+        //WebsiteUrl - Nullable field (users can decide to input website url or not)
+        [Display(Name = "Website Link")]
+        public string? WebsiteUrl { get; set; }
+
+
+        //WebsiteUsername - Field for users to enter the username for each website they want to save to the application
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Website Username")]
+        public string WebsiteUsername { get; set; }
+
+
+        //WebsitePassword - Field for users to enter the password for each website they want to save to the application
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Website Password")]
+        public string WebsitePassword { get; set; }
+
+
+        //DateCreated - Field to store the date each username and password was entered into the application
+        public DateTime DateCreated { get; set; }
+
+
+        //UserId - foreign key from parent table, User (to implement one-to-many relationship)
+        public int UserId { get; set; }
+
+
+        //Navigation property - parent reference (Each password credential has a user)
+        public User User { get; set; }
+
+    }
+}
